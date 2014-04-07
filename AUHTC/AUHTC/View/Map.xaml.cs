5337dd0d051fa511aa1f5962fc188cd7c0786e03
Riscue <ibrahimakyel94@gmail.com>
2014-04-08 01:38:01 +0300

    File İşlemi
    thread kullandım ekranda bindın gözükmesi için. Bide acayip bi regex
    yazdım :D

--- AUHTC/AUHTC/View/Map.xaml.cs
+++ AUHTC/AUHTC/View/Map.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
 
 namespace AUHTC.View
 {
@@ -19,6 +10,7 @@ namespace AUHTC.View
     /// </summary>
     public partial class Map : Window
     {
+        Thread thread;
         AnkaFelix Parent;
         public Map(AnkaFelix parent)
         {
@@ -27,26 +19,25 @@ namespace AUHTC.View
             Parent = parent;
         }
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-            Parent.Show();
-        }
-
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-            TextBox1.Text = "does it work";
-            App.MapModel.func();
-            TextBox1.Text = "it works";
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            screen.DataContext = App.MapModel;
+            App.MapModel.ReadFile = File.OpenText("a.txt");
+            thread = new Thread(new ThreadStart(delegate
+            {
+                while (true)
+                {
+                    if(!App.MapModel.ReadData())
+                        thread.Abort();
+                    Thread.Sleep(1);
+                }
+            }));
+            thread.Start();
         }
 
         private void Window_Unloaded(object sender, RoutedEventArgs e)
         {
+            thread.Abort();
+            App.MapModel.ReadFile = null;
             this.Close();
             Parent.Show();
         }
