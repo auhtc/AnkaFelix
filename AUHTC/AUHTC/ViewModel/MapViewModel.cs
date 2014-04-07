3eba2aea63bacb215867ed1e8223944af79179e3
auhtc <info@auhtc.com>
2014-04-07 17:37:44 +0300

    margin

--- AUHTC/AUHTC/ViewModel/MapViewModel.cs
+++ AUHTC/AUHTC/ViewModel/MapViewModel.cs
@@ -9,23 +9,41 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
 using System.Windows.Threading;
 
 namespace AUHTC.ViewModel
 {
-    public class MapViewModel : INotifyPropertyChanged
+    public class MapViewModel : INotifyPropertyChanged 
     {
         Regex regex = new Regex(@"([A-Z]{9},[0-9][0-9][0-9])");
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private MapModel mapModel;
+        //private MapModel mapModel;
+
+        private Thickness margin;// = new MapModel();
+        public Thickness Margin
+        {
+            get { return margin; }
+            set
+            {
+                if (value != margin)
+                {
+                    margin = value;
+                    NotifyPropertyChanged("Margin");
+                }
+            }
+        }
+
+
 
         private int temp_x = 0;
         private int temp_y = 0;
 
-        private ObservableCollection<MapModel> dataCollection = new ObservableCollection<MapModel>();
-        public ObservableCollection<MapModel> DataCollection
+        private MapModel dataCollection;// = new MapModel();
+        public MapModel DataCollection
         {
             get { return dataCollection; }
             set
@@ -44,13 +62,14 @@ namespace AUHTC.ViewModel
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-
         public void func()
         {
-            mapModel = new MapModel();
-            mapModel.KoorX = temp_x++;
-            mapModel.KoorY = temp_y++;
-            DataCollection.Add(mapModel);
+            margin.Left = temp_x;
+            margin.Top = temp_y;
+            dataCollection = new MapModel();
+            dataCollection.KoorX = temp_x++;
+            dataCollection.KoorY = temp_y++;
+            //DataCollection.Add(mapModel);
         }
     }
 }
