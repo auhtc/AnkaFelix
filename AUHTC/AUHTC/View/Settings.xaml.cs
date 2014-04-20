24a9f46d18081cb951f0107d54e73729d0abf41e
Riscue <ibrahimakyel94@gmail.com>
2014-04-20 17:06:25 +0300

    Settings sayfası tasarım yapıldı (kodlar comment)

--- AUHTC/AUHTC/View/Settings.xaml.cs
+++ AUHTC/AUHTC/View/Settings.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +23,29 @@ namespace AUHTC.View
         {
             InitializeComponent();
             Parent = parent;
+
+            AyarOku();
+        }
+
+        private void AyarOku()
+        {
+            //StreamReader ayaroku;
+            //string yazi = "0";
+            //ayaroku = File.OpenText(dosya);
+            //do
+            //{
+            //    yazi = ayaroku.ReadLine();
+            //    if (yazi == null) { break; }
+            //    if (yazi.Substring(0, 6) == "MapLoc") { MapImage.Source = yazi.Substring(7, yazi.Length - 7); label1.Text = yazi.Substring(7, yazi.Length - 7); continue; }
+            //    if (yazi.Substring(0, 7) == "MapName") { mapnameTextBox.Text = yazi.Substring(8, yazi.Length - 8); continue; }
+            //    if (yazi.Substring(0, 7) == "OffSet1") { offsetTextbox1.Text = yazi.Substring(8, yazi.Length - 8); continue; }
+            //    if (yazi.Substring(0, 7) == "OffSet2") { offsetTextbox2.Text = yazi.Substring(8, yazi.Length - 8); continue; }
+            //} while (yazi != null);
+
+            //ayaroku.Close();
+
+
+            // Bu fonksiyon regex öğrenmeden önce yazdğım düz mantık :D
         }
 
         private void PortNamesCombobox_Loaded(object sender, RoutedEventArgs e)
@@ -38,6 +63,14 @@ namespace AUHTC.View
             Properties.Settings.Default.DefaultPortName = PortNamesCombobox.SelectedItem.ToString();
             Properties.Settings.Default.DefaultBaudRate = Convert.ToInt32(BaudRatesCombobox.SelectedItem);
             Properties.Settings.Default.Save();
+
+            //StreamWriter ayaryaz;
+            //ayaryaz = File.CreateText(file);
+            //ayaryaz.WriteLine("MapLoc=" + MapImage.Source.ToString());
+            //ayaryaz.WriteLine("MapName=" + mapnameTextBox.Text);
+            //ayaryaz.WriteLine("OffSet1=" + offsetTextbox1.Text); //İçinde sayi,sayi şeklinde veri olmalı regex koysak mı?
+            //ayaryaz.WriteLine("OffSet2=" + offsetTextbox2.Text); //İçinde sayi,sayi şeklinde veri olmalı regex koysak mı?
+            //ayaryaz.Close();
         }
 
         private void OkeyButton_Click(object sender, RoutedEventArgs e)
@@ -52,6 +85,43 @@ namespace AUHTC.View
         {
             this.Close();
             Parent.IsEnabled = true;
+        }
+
+        private void DeleteMap_Click(object sender, RoutedEventArgs e)
+        {
+            // Database den şuan açık map silinecek.
+            // NewMap_Click çalışacak
+        }
+
+        private void EditMap_Click(object sender, RoutedEventArgs e)
+        {
+            //mapnameTextBox.IsEnabled = true;
+            //offsetTextbox1.IsEnabled = true;
+            //offsetTextbox2.IsEnabled = true;
+
+            //OpenFileDialog resimac = new OpenFileDialog();
+            //resimac.Title = "Lütfen ayar dosyasını nereye kaydedeceğinizi seçin.";
+            //resimac.Filter = "PNG Resim(*.png)|*.png|JPEG Resim(*.jpg)|*.jpg";
+            //resimac.FilterIndex = 2;
+            //resimac.ShowDialog();
+            //if (resimac.FileName.ToString() != "")
+            //{
+            //    MapImage.Source.SetValue(resimac.FileName);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Bir dosya seçmediniz.");
+            //}
+        }
+
+        private void NewMap_Click(object sender, RoutedEventArgs e)
+        {
+            //MapImage.Source.SetValue((DependencyProperty)null, null);
+            //mapnameTextBox.Text = "";
+            //offsetTextbox1.Text = "";
+            //offsetTextbox2.Text = "";
+            //Yeni harita oluşturucaz.. Map için png seçici bir dialog açıcaz.
+            // EditMap_Click çalışacak
         }
     }
 }
